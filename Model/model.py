488b84252eddf1fa1cea5131efebce295631ae45
Eunjin An <112074208+EunjinAn@users.noreply.github.com>
2024-11-06 13:26:17 +0100

    Update model.py

--- Model/model.py
+++ Model/model.py
@@ -13,7 +13,7 @@ from models.embed import DataEmbedding
 class Informer(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len,
                  factor=5, d_model=512, n_heads=8, e_layers=3, d_layers=2, d_ff=512,
-                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                 dropout=0.0, attn='prob', embed='fixed', freq='D', activation='gelu',
                  output_attention=False, distil=True, mix=True,
                  device=torch.device('cuda:0')):
         super(Informer, self).__init__()
@@ -89,7 +89,7 @@ class Informer(nn.Module):
 class InformerStack(nn.Module):
     def __init__(self, enc_in, dec_in, c_out, seq_len, label_len, out_len,
                  factor=5, d_model=512, n_heads=8, e_layers=[3, 2, 1], d_layers=2, d_ff=512,
-                 dropout=0.0, attn='prob', embed='fixed', freq='h', activation='gelu',
+                 dropout=0.0, attn='prob', embed='fixed', freq='D', activation='gelu',
                  output_attention=False, distil=True, mix=True,
                  device=torch.device('cuda:0')):
         super(InformerStack, self).__init__()
